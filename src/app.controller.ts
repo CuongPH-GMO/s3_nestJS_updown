f6d262c8ff60decfad6dc9b13db43d74738a8e9e
phancuong1234 <pvtho1111@gmail.com>
2020-07-09 00:10:06 +0700

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, UploadedFile, UseInterceptors, Res, Query, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 import {FileInterceptor} from '@nestjs/platform-express';
+import { response } from 'express';
 @Controller()
 export class AppController {
   
@@ -10,17 +11,35 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  @Get('list')
+  async getList() {
+    const list = await this.appService.getList();
+    console.log(list)
+    for (let index = 0; index < list['Contents'].length; index++) {
+      console.log(list['Contents'][index]['Key'])        
+    }
+  }
   @Get('download') 
   download(@Query('filename') filename, @Res() res) {
     res.setHeader('Content-Type', 'application/octet-stream');
     res.attachment(filename);
     this.appService.download(filename).createReadStream().pipe(res);
-    return res;
+    return res.end();
   }
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
-  async uploadFile(@UploadedFile() file: any) {
-    console.log(file)
-    return await this.appService.uploadFile(file);
+  async uploadFile(@UploadedFile() file: any, @Res() res) {
+    this.appService.uploadFile(file).then(data => {
+      return res.json({
+        code: 200,
+        message: 'success',
+        data
+      })
+    }).catch(e => {
+      return res.json({
+        code: e.statusCode,
+        message: e,
+      })
+    });
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -4,36 +4,31 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   
-  AWS_S3_BUCKET_NAME = process.env.S3_BUCKET;
+  AWS_S3_BUCKET_NAME = process.env.S3_BUCKET || 'apkfolder';
   s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || 'AKIAJEKXGZYVNKC5UERA',
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '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'
   })
   getHello(): string {
     return 'Hello World!';
   }
 
-  public async uploadFile(file: any):Promise<string> {
+  public async uploadFile(file: any):Promise<any> {
     const data = await this.s3.putObject({
       Bucket: this.AWS_S3_BUCKET_NAME,
       Key: file.originalname,
       ACL: 'public-read',
       Body: file.buffer,
-    }).promise().then(
-      data => {
-        console.log(data);
-        return ;
-      },
-      err => {
-        console.log(err);
-        return err;
-      },
-    );
+    }).promise();
     return data;
   }
 
   public download(key) {
     const bucketParams = {Key: key, Bucket: this.AWS_S3_BUCKET_NAME};
     return this.s3.getObject(bucketParams);
+  }
+
+  public getList() {
+    return this.s3.listObjects({Bucket: this.AWS_S3_BUCKET_NAME}).promise();
   }
 }
