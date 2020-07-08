2b7750d4bb07a10378ee795acfaf268b52641c9a
phancuong1234 <pvtho1111@gmail.com>
2020-07-08 23:37:02 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 const express = require('express');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(express.static('./asset'));
   app.setGlobalPrefix('api');
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
